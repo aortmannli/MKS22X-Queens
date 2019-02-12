8e46229c41e827eb84b1101c015ba919a7ce85dd
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-02-11 22:11:52 -0500

    trying to figure out this text thing??

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+  public static void main(String[] args) {
+    QueenBoard x = new QueenBoard(8);
+    System.out.println(x.countSolutions());
+    System.out.println(x);
+  }
+}
